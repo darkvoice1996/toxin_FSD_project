ea95bd0bf1bded9845bde7337cb26ef023412994
Alexandr Spiridonov <darkvoice1996@gmail.com>
2021-01-22 19:12:23 +0300

    Pagination: go to previous button added

--- src/common.block/pagination/pagination.js
+++ src/common.block/pagination/pagination.js
@@ -18,7 +18,7 @@ $(function () {
     container.pagination({
         dataSource: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
         pageRange: 1,
-        showPrevious: false,
+        showPrevious: true,
         pageSize: 2,
         autoHidePrevious: true,
         autoHideNext: false,
