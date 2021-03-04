5ebf087cc17476edb4f877749ae32cf4afc114b4
Alexandr Spiridonov <darkvoice1996@gmail.com>
2021-03-04 18:53:27 +0300

    iss7: value generator for dropdown created

--- src/common.block/dropdown/dropdown.js
+++ src/common.block/dropdown/dropdown.js
@@ -6,6 +6,25 @@ for (let i = 0; i < dropdowns.length; i++) {
 
     toggleContent(input, content)
 
+    content.addEventListener('click', function (event) {
+        pasteInputValue(input)
+    })
+
+    function pasteInputValue(input) {
+        let titles = dropdowns[i].querySelectorAll('.dropdown-counter__title')
+        let values = dropdowns[i].querySelectorAll('.dropdown-counter__value')
+
+        let result = ''
+
+        for (let i = 0; i < titles.length && i < values.length; i++) {
+            if (!(values[i].innerText === '0')) {
+                if (result !== '') result += ', '
+                result += titles[i].innerText.toLowerCase() + ': ' + values[i].innerText
+            }
+        }
+
+        input.value = result
+    }
     function toggleContent(input, content) {
         input.onfocus = () => content.classList.remove('dropdown__content_hidden')
 
