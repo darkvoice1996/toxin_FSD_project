739c84bf3298218f0c7e32787a7c955c7f43abb5
Alexandr Spiridonov <darkvoice1996@gmail.com>
2021-03-05 00:49:21 +0300

    iss7: script for masked input is written

--- src/common.block/input/input_masked.js
+++ src/common.block/input/input_masked.js
@@ -1,4 +1,27 @@
-import 'jquery.maskedinput/src/jquery.maskedinput'
-jQuery(function($){
-    $(".input_masked").mask("99.99.9999",{placeholder:"ДД.ММ.ГГГГ"});
-});
+maskBirthDay(document.querySelector('.input_masked'))
+
+function maskBirthDay(input) {
+    let mask = /[0-3][0-9]\.[0-1][0-9]\.[1-2][09][0-9][0-9]/
+
+    input.addEventListener('input', function (event) {
+        checkOnRegExp(/[0-9]/)
+        // console.log(input.value.length)
+        // input.blur()
+
+        if (input.value.length === 8) {
+            input.value = setDots(input.value)
+            input.blur()
+        }
+    })
+
+    function checkOnRegExp(regExp) {
+        if (!(regExp.test(event.data))) {
+            input.value = input.value.slice(0, -1)
+        }
+    }
+
+    function setDots(value) {
+        return value.slice(0, 2) + '.' + value.slice(2, 4) + '.' + value.slice(4)
+    }
+
+}
